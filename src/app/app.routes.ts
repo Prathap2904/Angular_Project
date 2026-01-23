402f372a2da435b6a6f158aaa98ce2f40a5e2111
Sindhuja573 <sindhuja2k16@gmail.com>
2026-01-23 17:48:30 +0530

    ag-grid

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Login } from './auth/login/login';
 import { Register } from './auth/register/register';
 import { Flash } from './auth/flash/flash';
 import { Product } from './products/product/product';
+import { Productgrid } from './productgrid/productgrid';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
@@ -11,6 +12,7 @@ export const routes: Routes = [
   { path: 'auth/register', component: Register },
   { path: 'auth/flash', component: Flash },
    { path: 'products', component: Product },
+   {path:'productgrid',component:Productgrid},
 
   { path: '**', redirectTo: 'auth/login' }
 
